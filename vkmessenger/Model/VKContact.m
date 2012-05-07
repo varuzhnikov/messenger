e039cab0ab7003931837fc60bae41d1644d93632
Vitaly Ruzhnikov <varuzhnikov@gmail.com>
2012-05-07 13:31:18 +0400

    Changed bundle identifier

--- vkmessenger/Model/VKContact.m
+++ vkmessenger/Model/VKContact.m
@@ -33,8 +33,8 @@
     }
 
     if ([NSString isEmpty:self.firstName]) {
-            return self.lastName;
-        }
+        return self.lastName;
+    }
 
     if ([NSString isEmpty:self.lastName]) {
         return self.firstName;
